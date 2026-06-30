2420ed022ccb671a2500dd0eb212826524c10345
Tanmay Soni <tanmaysoni354@gmail.com>
2026-06-30 19:58:13 +0530

    Added error handler functions & also verify password

---
+++ Includes/MVC_logIn/login_model.inc.php
@@ -0,0 +1,23 @@
+<?php 
+declare(strict_types=1);
+
+function get_username_login(object $pdo , string $username){
+
+    $query = "SELECT USERNAME FROM accounts WHERE USERNAME = :username;";
+    $statement = $pdo->prepare($query);
+    $statement->bindParam(":username" , $username);
+    $statement->execute();
+
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+function get_password_login(object $pdo , string $password){
+    
+ $query = "SELECT PASSWORD FROM accounts WHERE PASSWORD = :password;";
+    $statement = $pdo->prepare($query);
+    $statement->bindParam(":password" , $password);
+    $statement->execute();
+
+    return $statement->fetch(PDO::FETCH_ASSOC);
+
+}
