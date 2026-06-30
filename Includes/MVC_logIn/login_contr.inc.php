9615acbb6d843286110ee59b7c5a17372bd0336f
Tanmay Soni <tanmaysoni354@gmail.com>
2026-06-30 19:59:07 +0530

    Accessed password and username from database

---
+++ Includes/MVC_logIn/login_contr.inc.php
@@ -0,0 +1,35 @@
+<?php 
+
+declare(strict_types=1);
+
+function is_input_empty(string $username, string $password){
+    if(empty($username) || empty($password)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function is_username_incorrect(object $pdo , string $username ){
+
+        if(!get_username_login($pdo , $username)){
+            return true;
+        }
+        else{
+            return false;
+        }
+}
+
+function is_password_incorrect(object $pdo , string $password){
+
+        $D_password = get_password_login($pdo , $password);
+
+        if(!password_verify($password , $D_password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+}
