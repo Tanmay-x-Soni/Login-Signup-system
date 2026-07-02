539529aa0b3377f51fd944074dfac21d690461e3
Tanmay Soni <tanmaysoni354@gmail.com>
2026-07-02 13:44:25 +0530

    Login page handler created & also error handler

--- Includes/logIn.inc.php
+++ Includes/logIn.inc.php
@@ -5,33 +5,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-    if (empty($username) || empty($password)) {
-        header("Location: ../logIn.php");
-        exit();
-    } else {
+    
         try {
             require "dbh.inc.php";
+            require_once __DIR__ . '/MVC_logIn/login_model.inc.php';
+            require_once __DIR__ . '/MVC_logIn/login_contr.inc.php';
 
-            $query = "SELECT * FROM accounts WHERE USERNAME = :username;";
-            $statement = $pdo->prepare($query);
-            $statement->bindParam(":username", $username);
-            $statement->execute();
+            $login_errors =[];
 
-            $result = $statement->fetch(PDO::FETCH_ASSOC);
-
-            $pdo = null;
-            $statement = null;
-
-            if ($result && password_verify($password, $result['PASSWORD'])) {
-                header("Location: ../logInResults.php");
-                exit();
-            } else {
-                header("Location: ../logIn.php");
-                exit();
+            if(is_input_empty($username , $password)){
+                $login_errors['empty_input'] ="Fill in all the fields!";
             }
+
+            if(is_username_incorrect($pdo , $username )){
+                $login_errors['incorrect_username'] = "your username is incorrect!";
+            }
+
+            if(is_password_incorrect($pdo , $password)){
+                $login_errors['incorrect_password'] = "Password is incorrect!";
+            }
+
+             require_once '../Includes/config_sessions.inc.php';
+
+             if($login_errors){
+                $_SESSION["login_errors"] = $login_errors;
+
+                header("Location: ../logIn.php");
+                die();
+             }
+
+             unset($_SESSION["login_errors"]);
+
+             header("Location: ../logIn.php");
+            $statement = null;
+            $pdo = null;
+            die();
+
+           
 
         } catch (PDOException $e) {
             die("Query Failed: " . $e->getMessage());
         }
     }
+    else {
+    header("Location: ../logIn.php");
+        die();
 }
