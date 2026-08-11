f2294a5121ddf8ebcbe2693af5307eba41a91767
Tanmay Soni <tanmaysoni354@gmail.com>
2026-08-11 17:39:42 +0530

    completed profile setup handler file

---
+++ Includes/completeProfile.inc.php
@@ -0,0 +1,98 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+$fullName = $_POST["full_name"];
+$phoneNumber = $_POST["phone_number"];
+$gender = $_POST["gender"];
+$DOB = $_POST["date_of_birth"];
+$country = $_POST["country"];
+$bio = $_POST["bio"];
+
+try{
+    require 'dbh.inc.php';
+     require_once __DIR__ . '/MVC_profile/profile_model.inc.php';
+     require_once __DIR__ . '/MVC_profile/profile_contr.inc.php';
+
+     $profile_errors = [];
+
+
+      $fullName_error = get_fullname_error($fullName);
+      if($fullName_error){
+        $profile_errors['fullName_error'] = $fullName_error;
+      }
+
+      $phoneNumber_error = get_phone_error($phoneNumber);
+      if($phoneNumber_error){
+        $profile_errors['phoneNumber_error'] = $phoneNumber_error;
+      }
+    
+      $gender_error = get_gender_error($gender);
+      if($gender_error){
+        $profile_errors['gender_error'] = $gender_error;
+      }
+
+      $dob_error = get_dob_error($DOB);
+      if($dob_error){
+        $profile_errors['dob_error'] = $dob_error;
+      }
+
+      $country_error = get_country_error($country);
+      if($country_error){
+        $profile_errors['country_error'] = $country_error;
+      }
+
+      $bio_error = get_bio_error($bio);
+      if($bio_error){
+        $profile_errors['bio_error'] = $bio_error;
+      }
+
+      require_once '../Includes/config_sessions.inc.php';
+
+      if($profile_errors){
+        $_SESSION["profile_errors"] = $profile_errors;
+
+        $profileData =[
+            "full_name" => $fullName ,
+            "phone_number" => $phoneNumber , 
+            "gender" => $gender , 
+            "date_of_birth" => $DOB  , 
+            "country" =>  $country, 
+            "bio" => $bio 
+        ];
+
+        $_SESSION["profile_data"] = $profileData;
+        header("Location: ../completeProfile.php");
+        die();
+      }
+
+      unset($_SESSION["profile_errors"]);
+      unset($_SESSION["profile_data"]);
+      set_profile_data($pdo , $fullName , $phoneNumber , $gender , $DOB , $country , $bio);
+
+     header("Location: ../completeProfile.php");
+        $statement = null;
+        $pdo = null;
+        die();
+
+
+
+
+
+} catch (PDOException $e){
+    die("Querry failed!" . $e->getMessage());
+}
+
+
+
+} else {
+    header("Location:../completeProfile.php");
+    die();
+}
+
+
+
+
+
+?>
