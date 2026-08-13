55df030df11618dc5c71d0327e62ebb6755c58a8
Tanmay Soni <tanmaysoni354@gmail.com>
2026-08-13 13:27:04 +0530

    Created basic updation query for profile page

--- Includes/MVC_profile/profile_model.inc.php
+++ Includes/MVC_profile/profile_model.inc.php
@@ -3,8 +3,29 @@
 declare(strict_types=1);
 
 
-function set_profile_data(object $pdo , string $fullName , string $phoneNumber ,string $gender ,string $DOB , string $country ,string $bio){
+function set_profile_data(object $pdo , string $fullName , string $phoneNumber ,string $gender ,string $DOB , string $country ,string $bio ,string $userId){
 
+        $query = "UPDATE Accounts 
+                   SET FULL_NAME = :full_name ,
+                       PHONE = :phone ,
+                       GENDER = :gender , 
+                       DOB = :dob ,
+                       COUNTRY = :country ,
+                       BIO = :bio 
+                    WHERE ID = :id;
+                   ";
+
+        $statement = $pdo -> prepare($query);
+        
+        $statement->bindparam( ":full_name" , $fullName);        
+        $statement->bindparam( ":phone" , $phoneNumber);        
+        $statement->bindparam( ":gender" , $gender);        
+        $statement->bindparam( ":dob" , $DOB);        
+        $statement->bindparam( ":country" , $country );        
+        $statement->bindparam( ":bio" , $bio);
+        $statement->bindparam( ":id" , $userId , PDO::PARAM_INT);
+
+        $statement -> execute();
 }
 
 ?>
